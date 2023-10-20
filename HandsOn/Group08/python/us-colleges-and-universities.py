0e7eff567f93b09910bbcb24b08a1791e4be58d3
RaulMoldes <raul.moldes.work@gmail.com>
2023-10-20 17:37:13 +0200

    Arreglada ontología y mapping us-colleges-and-universities

--- HandsOn/Group08/python/us-colleges-and-universities.py
+++ HandsOn/Group08/python/us-colleges-and-universities.py
@@ -1,7 +1,46 @@
-import morph_kgc
-from rdflib import Graph, Namespace, Literal, XSD
-from rdflib.namespace import RDF, RDFS, FOAF
 from rdflib.plugins.sparql import prepareQuery
+from rdflib.namespace import RDF, RDFS, FOAF
+from rdflib import Graph, Namespace, Literal, XSD
+import morph_kgc
+import pandas as pd
+
+# %% Eliminar duplicados
+path = "HandsOn/Group08/csv2/us-colleges-and-universities.csv"
+
+csv = pd.read_csv(path)
+
+duplicados = csv['IPEDSID'].duplicated(keep=False)
+print("Number of dups", len(csv[duplicados]))
+
+print("Old:", len(csv))
+
+csv = csv.drop_duplicates(subset="IPEDSID", keep="last")
+
+
+print("New:", len(csv))
+print(csv.keys())
+
+# %% Poner los valores de las columnas en un formato adecuado para poder realizar el mapping
+
+
+columna = csv['NAME']
+columna2 = csv['CITY']
+
+
+columna = columna.str.title()
+columna2 = columna2.str.title()
+columna4 = 'United States'
+
+csv['NAME'] = columna
+csv['CITY'] = columna2
+csv['COUNTRY'] = columna4
+
+
+csv.to_csv(
+    'HandsOn/Group08/csv2/us-colleges-and-universities-modified.csv', index=False)
+
+
+# %% Crear grafo en formato RDF
 print(RDF)
 
 config = """
@@ -11,6 +50,9 @@ config = """
     mappings= HandsOn/Group08/mappings/us-colleges-and-universities/us-colleges-and-universities.ttl
          """
 g = morph_kgc.materialize(config)
+
+
+# %% Crear consulta SPARQL
 g.namespace_manager.bind('ns', Namespace(
     "http://www.semanticweb.org/upm/opendata/group08/ontologies/ns#"), override=False)
 g.namespace_manager.bind('schema', Namespace(
