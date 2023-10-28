83e2bb81b823478319f99bf92691fd4adf0d66ab
RaulMoldes <raul.moldes.work@gmail.com>
2023-10-28 15:42:02 +0200

    Solucionado problema URIS

--- HandsOn/Group08/python/convert2RDF.py
+++ HandsOn/Group08/python/convert2RDF.py
@@ -7,5 +7,5 @@ from globals import CONFIG_MAPPING, OUT_GRAPH
 g = morph_kgc.materialize(CONFIG_MAPPING)
 
 # Write Graph
-with open(OUT_GRAPH, "w") as f:
+with open(OUT_GRAPH, "w", encoding='utf8') as f:
     f.write(g.serialize(format="ttl"))
--- HandsOn/Group08/python/unquote_uris.py
+++ HandsOn/Group08/python/unquote_uris.py
@@ -5,9 +5,18 @@ from urllib.parse import unquote
 df = pd.read_csv('csv/us-colleges-and-universities-final-with-links.csv')
 
 # Decodificar las URIs
-df['same-as_wikidata_country'] = df['same_as_wikidata_country'].apply(
-    lambda x: unquote(x))
+df['same_as_wikidata_country'] = df['same_as_wikidata_country'].str.replace(
+    'https://wikidata.org/entity/', '')
 
-# Guardar el dataframe decodificado en un nuevo archivo csv
+df['same_as_wikidata_city'] = df['same_as_wikidata_city'].str.replace(
+    'https://wikidata.org/entity/', '')
+
+df['same_as_wikidata_state'] = df['same_as_wikidata_state'].str.replace(
+    'https://wikidata.org/entity/', '')
+
+df['same_as_wikidata_name'] = df['same_as_wikidata_name'].str.replace(
+    'https://wikidata.org/entity/', '')
+
+
 df.to_csv(
     'csv/us-colleges-and-universities-final-with-links-unquoted.csv', index=False)
