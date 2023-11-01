8fd4d5c04a83983011661dbce6d51cf0b6237c65
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2023-11-01 23:25:16 +0100

    refactor

--- HandsOn/Group08/python/convert2RDF.py
+++ HandsOn/Group08/python/convert2RDF.py
@@ -1,20 +1,20 @@
 
 import morph_kgc
-from globals import CONFIG_MAPPING, OUT_GRAPH, OUT_GRAPH_LINKS
+from globals import CONFIG_MAPPING, CONFIG_MAPPING_LINKS, OUT_GRAPH, OUT_GRAPH_LINKS
 
 
-# Without linking 
-
-
-# Create a Graph
+# Without linking
 g = morph_kgc.materialize(CONFIG_MAPPING)
 
-# Write Graph
 with open(OUT_GRAPH, "w", encoding='utf8') as f:
     f.write(g.serialize(format="nt"))
 
 
-exit()
-#withlinking 
+print("\n"*2, "-"*20, "\n"*2)
+
+# With links
+
+g_with_links = morph_kgc.materialize(CONFIG_MAPPING_LINKS)
+
 with open(OUT_GRAPH_LINKS, "w", encoding='utf8') as f:
     f.write(g.serialize(format="turtle"))
