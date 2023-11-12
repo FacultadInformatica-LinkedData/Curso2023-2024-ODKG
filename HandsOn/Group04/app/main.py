ebd2d847fcb0fb5115554daaa3f2a6cbb4de1995
Guillermo <guilogar42@gmail.com>
2023-11-12 18:18:41 +0100

    cambios añadidos

--- HandsOn/Group04/app/main.py
+++ HandsOn/Group04/app/main.py
@@ -4,20 +4,24 @@ from rdflib import Graph, Namespace
 app = Flask(__name__)
 
 # Cargar el archivo .ttl al grafo RDF
-g = Graph()
-g.parse("./output-datasets-with-links-updated.ttl", format="turtle")
+graph = Graph()
+graph.parse("./output-datasets-with-links-updated.ttl", format="turtle")
 ns = Namespace("http://www.madculturalevents.es/group04/ontology/madculturalevents#")
 schema = Namespace("http://schema.org/")
 
 @app.route('/')
 def home():
-    query = """PREFIX ns0: <http://www.madculturalevents.es/group04/ontology/madculturalevents#>
-    SELECT ?event ?eventPrice ?eventAccessibility ?eventPlace WHERE {
-        ?event a ns0:CulturalEvent ;
+    query = """
+    PREFIX ns0: <http://www.madculturalevents.es/group04/ontology/madculturalevents#>
+    PREFIX schema: <http://schema.org/> 
+    SELECT ?name ?eventPrice ?eventAccessibility ?eventPlace WHERE {
+      ?event a schema:Event ;
+               schema:name ?name ;
                ns0:price ?eventPrice ;
                ns0:accesibility ?eventAccessibility ;
                ns0:hasPlace ?eventPlace  .
-    } """
+    }
+    """
     result = execute_sparql_query(query)
     prices = get_prices()
     accessibilities = get_accessibilities()
@@ -25,14 +29,17 @@ def home():
     return render_template('index.html', query=query, result=result, prices=prices, accessibilities=accessibilities,
                            districts=districts)
 
-@app.route('/sparql', methods=['POST'])
-def sparql_query():
-    query = request.form['query']
-    result = execute_sparql_query(query)
-    return render_template('./sparql.html', query=query, result=result)
+def execute_sparql_query(query):
+    results = graph.query(query)
 
+    result_values = []
+    for row in results:
+        result_row = []
+        for value in row:
+            result_row.append(value.toPython() if hasattr(value, 'toPython') else value)
+        result_values.append(result_row)
 
-
+    return result_values
 
 def get_prices():
     query = '''
@@ -43,7 +50,7 @@ def get_prices():
     }
     '''
     # Almacena los resultados en una lista
-    prices_list = [row.price for row in g.query(query, initNs={"ns": ns, "schema": schema})]
+    prices_list = [row.price for row in graph.query(query, initNs={"ns": ns, "schema": schema})]
 
     # Visualiza los resultados
     print("Precios:", prices_list)
@@ -59,7 +66,7 @@ def get_districts():
       }
     '''
     # Almacena los resultados en una lista
-    districts_list = [row.district for row in g.query(query, initNs={"ns": ns, "schema": schema})]
+    districts_list = [row.district for row in graph.query(query, initNs={"ns": ns, "schema": schema})]
 
     # Visualiza los resultados
     print("Districts:", districts_list)
@@ -74,7 +81,7 @@ def get_accessibilities():
     }
     '''
     # Almacena los resultados en una lista
-    accessibility_list = [row.accessibility for row in g.query(query, initNs={"ns": ns, "schema": schema})]
+    accessibility_list = [row.accessibility for row in graph.query(query, initNs={"ns": ns, "schema": schema})]
 
     # Combina todas las cadenas en una sola
     combined_accessibility = ",".join(accessibility_list)
@@ -86,19 +93,25 @@ def get_accessibilities():
     print("Accessibility:", list(unique_accessibility))
     return list(unique_accessibility)
 
+@app.route('/search')
+def search():
+    search_value = request.args.get('search_value', '')
+    q1 = f"""
+    PREFIX ns0: <http://www.madculturalevents.es/group04/ontology/madculturalevents#>
+    PREFIX schema: <http://schema.org/> 
 
-def execute_sparql_query(query):
-    results = g.query(query)
+    SELECT ?name ?eventPrice ?eventAccessibility ?eventPlace WHERE {{
+        ?event a schema:Event .
+        ?event schema:name ?name .
+        ?event ns0:price ?eventPrice .
+        ?event ns0:accesibility ?eventAccessibility .
+        ?event ns0:hasPlace ?eventPlace .
+        FILTER (CONTAINS(?name, "{search_value}"))
+    }}
+    """
+    result = execute_sparql_query(q1)
+    return render_template('index.html', result=result)
 
-    # Obtener los valores reales de los objetos
-    result_values = []
-    for row in results:
-        result_row = []
-        for value in row:
-            # Convertir objetos RDF a valores de Python
-            result_row.append(value.toPython() if hasattr(value, 'toPython') else value)
-        result_values.append(result_row)
-    return result_values
 
 if __name__ == '__main__':
     app.run(debug=True)
