209bc474a23f409875b76aa16f35eaee228c440f
Guillermo <guilogar42@gmail.com>
2023-11-13 10:56:04 +0100

    funcionalidad mostrar información sobre facility

--- HandsOn/Group04/app/main.py
+++ HandsOn/Group04/app/main.py
@@ -8,29 +8,20 @@ graph = Graph()
 graph.parse("./output-datasets-with-links-updated.ttl", format="turtle")
 ns = Namespace("http://www.madculturalevents.es/group04/ontology/madculturalevents#")
 schema = Namespace("http://schema.org/")
+geonames = Namespace("http://www.geonames.org/ontology#")
+esadm = Namespace("http://vocab.linkeddata.es/datosabiertos/def/sector-publico/territorio#")
+
 
 @app.route('/')
 def home():
-    query = """
-    PREFIX ns0: <http://www.madculturalevents.es/group04/ontology/madculturalevents#>
-    PREFIX schema: <http://schema.org/> 
-    SELECT ?name ?eventPrice ?eventAccessibility ?eventPlace WHERE {
-      ?event a schema:Event ;
-               schema:name ?name ;
-               ns0:price ?eventPrice ;
-               ns0:accesibility ?eventAccessibility ;
-               ns0:hasPlace ?eventPlace  .
-    }
-    """
-    result = execute_sparql_query(query)
+    result = get_event_list()
     prices = get_prices()
     accessibilities = get_accessibilities()
     districts = get_districts()
-    return render_template('index.html', query=query, result=result, prices=prices, accessibilities=accessibilities,
-                           districts=districts)
+    return render_template('index.html', result=result, prices=prices, accessibilities=accessibilities, districts=districts)
 
 def execute_sparql_query(query):
-    results = graph.query(query)
+    results = graph.query(query, initNs={"ns": ns, "schema": schema, "geonames": geonames, "esadm": esadm})
 
     result_values = []
     for row in results:
@@ -40,6 +31,27 @@ def execute_sparql_query(query):
         result_values.append(result_row)
 
     return result_values
+
+def get_event_list(search_value=""):
+    query = f"""
+     SELECT ?event ?name ?description ?eventPrice ?eventAccessibility ?facilityName ?eventPlace ?startDate ?endDate ?audienceType ?eventType WHERE {{
+        ?event a schema:Event ;
+            schema:name ?name ;
+            schema:description ?description ;
+            ns:price ?eventPrice ;
+            ns:accesibility ?eventAccessibility ;
+            ns:hasPlace ?eventPlace ;
+            schema:startDate ?startDate ;
+            schema:endDate ?endDate ;
+            ns:hasAudienceType ?audienceType ;
+            ns:hasEventType ?eventType .
+        ?eventPlace a ns:Facility ;
+            ns:facilityName ?facilityName .
+        FILTER (CONTAINS(?name, "{search_value}"))
+    }}
+    """
+    result = execute_sparql_query(query)
+    return result
 
 def get_prices():
     query = '''
@@ -53,7 +65,7 @@ def get_prices():
     prices_list = [row.price for row in graph.query(query, initNs={"ns": ns, "schema": schema})]
 
     # Visualiza los resultados
-    print("Precios:", prices_list)
+    # print("Precios:", prices_list)
     return prices_list
 
 
@@ -69,7 +81,7 @@ def get_districts():
     districts_list = [row.district for row in graph.query(query, initNs={"ns": ns, "schema": schema})]
 
     # Visualiza los resultados
-    print("Districts:", districts_list)
+    # print("Districts:", districts_list)
     return districts_list
 
 def get_accessibilities():
@@ -90,27 +102,61 @@ def get_accessibilities():
     unique_accessibility = set(combined_accessibility.split(','))
 
     # Visualiza los resultados
-    print("Accessibility:", list(unique_accessibility))
+    # print("Accessibility:", list(unique_accessibility))
     return list(unique_accessibility)
 
 @app.route('/search')
 def search():
     search_value = request.args.get('search_value', '')
-    q1 = f"""
-    PREFIX ns0: <http://www.madculturalevents.es/group04/ontology/madculturalevents#>
-    PREFIX schema: <http://schema.org/> 
+    result_list = get_event_list(search_value)
+    prices = get_prices()
+    accessibilities = get_accessibilities()
+    districts = get_districts()
+    return render_template('index.html', result=result_list, prices=prices, accessibilities=accessibilities, districts=districts)
 
-    SELECT ?name ?eventPrice ?eventAccessibility ?eventPlace WHERE {{
-        ?event a schema:Event .
-        ?event schema:name ?name .
-        ?event ns0:price ?eventPrice .
-        ?event ns0:accesibility ?eventAccessibility .
-        ?event ns0:hasPlace ?eventPlace .
-        FILTER (CONTAINS(?name, "{search_value}"))
+@app.route('/get_facility_info')
+def get_facility_info():
+    facility_id = request.args.get('facility_id', '')
+    facility = "http://www.madculturalevents.es/group04/resources/facility/" + facility_id
+
+    query = f"""
+     SELECT ?facilityName ?metro ?bus ?train ?url ?lat ?long ?street ?number ?districtName WHERE {{
+            <{facility}> a ns:Facility ;
+                ns:facilityName ?facilityName ;
+                ns:metro ?metro ;
+                ns:bus ?bus ;
+                ns:train ?train ;
+                ns:facilityUrl ?url ;
+                ns:ubicatedIn ?geometry ;
+                ns:hasAddress ?address .
+            ?geometry ns:hasLat ?lat ;
+                ns:hasLong ?long .
+            ?address ns:addressName ?street ;
+                ns:number ?number;
+                ns:belongsTo ?district .
+            ?district geonames:officialName ?districtName .
     }}
     """
-    result = execute_sparql_query(q1)
-    return render_template('index.html', result=result)
+    result = execute_sparql_query(query)
+
+    query = f"""
+    SELECT ?event ?name ?description ?eventPrice ?eventAccessibility ?facilityName ?eventPlace ?startDate ?endDate ?audienceType ?eventType WHERE {{
+        ?event a schema:Event ;
+            schema:name ?name ;
+            schema:description ?description ;
+            ns:price ?eventPrice ;
+            ns:accesibility ?eventAccessibility ;
+            ns:hasPlace ?eventPlace ;
+            schema:startDate ?startDate ;
+            schema:endDate ?endDate ;
+            ns:hasAudienceType ?audienceType ;
+            ns:hasEventType ?eventType ;
+            ns:hasPlace <{facility}> .
+    }}
+    """
+    events = execute_sparql_query(query)
+
+    return render_template('facility_info.html', result=result[0], events=events)
 
 
 if __name__ == '__main__':
