588b3795212b6ba0eae2886c48cff51ad8d0eec6
Jorge Dagor <dlgcjorge@gmail.com>
2023-11-14 01:00:14 +0100

    app(maps): added maps with the location of the events.

--- HandsOn/Group04/app/main.py
+++ HandsOn/Group04/app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-import requests
 from rdflib import Graph, Namespace
+import requests
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ ns = Namespace("http://www.madculturalevents.es/group04/ontology/madculturaleven
 schema = Namespace("http://schema.org/")
 territorio = Namespace("http://vocab.linkeddata.es/datosabiertos/def/sector-publico/territorio#")
 geonames = Namespace("http://www.geonames.org/ontology#")
+owl = Namespace("http://www.w3.org/2002/07/owl#")
 wdt = Namespace("http://www.wikidata.org/prop/direct#")
 wd=Namespace("http://www.wikidata.org/entity#")
 xsd=Namespace("http://www.w3.org/2001/XMLSchema#")
@@ -27,8 +28,10 @@ def home():
     facility_names = get_facility_names()
     audience_types = get_audience_types()
     event_types = get_event_types()
+    locations = get_locations(result)
     return render_template('index.html', result=result, prices=prices, accessibilities=accessibilities,
-                           district_names=district_names, metro_list = metro_list, facility_names=facility_names, audience_types=audience_types, event_types=event_types)
+                           district_names=district_names, metro_list = metro_list, facility_names=facility_names, 
+                           audience_types=audience_types, event_types=event_types, locations=locations)
 
 def execute_sparql_query(query):
     results = graph.query(query, initNs={"ns": ns, "schema": schema, "geonames": geonames, "territorio": territorio,"wdt":wdt,"wd":wd,"xsd":xsd})
@@ -63,6 +66,42 @@ def get_event_list(search_value=""):
     
     result = execute_sparql_query(query)
     return result
+
+
+
+def get_locations(results):
+    wikidata_query = 'https://query.wikidata.org/embed.html#%23defaultView%3AMap%0ASELECT%20*%20WHERE%20%7B%20'
+
+    i = 1
+    for event in results:
+        # print(event[0])
+        query = f'''
+        SELECT ?sameAs
+        WHERE {{
+            <{event[0]}> a schema:Event ;
+            ns:hasPlace ?place .
+            ?place ns:hasAddress ?address .
+            ?address owl:sameAs ?sameAs
+        }}
+        '''
+        locations = [row.sameAs for row in graph.query(query, initNs={"ns": ns, "schema": schema, "owl": owl})]
+
+
+        for row in locations:
+            if row is not None:
+                same_as_url = row
+                wikidata_id = same_as_url.split("/")[-1]
+                if wikidata_id:
+                    wikidata_query+=f'%0A%20%20OPTIONAL%20%7B%20wd%3A{wikidata_id}%20wdt%3AP625%20%3Fgeo{i}%20%7D'
+                    i += 1
+
+    wikidata_query+='%0A%7D'
+
+    return wikidata_query
+
+
+
+
 
 def get_prices():
     query = '''
