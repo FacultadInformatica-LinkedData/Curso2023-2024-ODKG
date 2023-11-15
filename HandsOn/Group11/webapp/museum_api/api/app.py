4dfd442c4d260e59cf72824f66dd643fdca09159
Konrad Owsikowski <konrad.owsikowski@gmail.com>
2023-11-16 00:57:28 +0100

    Added API for web application

---
+++ HandsOn/Group11/webapp/museum_api/api/app.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, FastAPI
+from museum_api.data.data_loader import get_data_graph_object
+from museum_api.api.artwork.router import artwork_router
+
+
+app = FastAPI(
+    title="Museum app REST API",
+    description="Museum app REST API",
+    version='0.1.0',
+    docs_url="/api/ui",
+    on_startup=[get_data_graph_object]
+)
+
+api_router = APIRouter(prefix='/api')
+api_router.include_router(artwork_router)
+
+app.include_router(api_router)
