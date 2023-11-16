a8608b30e9e4beff212ea2276a9d8c27df19d306
Sergio Marin Sanchez <serms1999@gmail.com>
2023-11-16 23:20:28 +0100

    Fix errors

--- Assignment4/Serms1999-22A305/Task07.py
+++ Assignment4/Serms1999-22A305/Task07.py
@@ -14,16 +14,15 @@ g.namespace_manager.bind('vcard', Namespace("http://www.w3.org/2001/vcard-rdf/3.
 g.parse(github_storage+"/rdf/example6.rdf", format="xml")
 #%% md
 # **TASK 7.1: List all subclasses of "LivingThing" with RDFLib and SPARQL**
+#%% md
+# SPARQL version
 #%%
-# TO DO
-# Visualize the results
-
 from rdflib.plugins.sparql import prepareQuery
 NS = Namespace("http://somewhere#")
 
 q1 = prepareQuery('''
     SELECT ?subclass WHERE {
-      ?subclass rdfs:subClassOf ns:LivingThing.
+      ?subclass rdfs:subClassOf* ns:LivingThing.
     }
     ''',
     initNs = {'rdfs':RDFS, 'ns':NS}
@@ -32,11 +31,26 @@ q1 = prepareQuery('''
 for r in g.query(q1):
     print(r.subclass)
 #%% md
+# 
+#%% md
+# For the RDFLib version we need to define a auxiliar function for the recursion
+#%%
+def get_subclasses(graph, parent_class):
+    subclasses_list = []
+    for s, _, _ in graph.triples((None, RDFS.subClassOf, parent_class)):
+        subclasses_list += [s]
+        subclasses_list += get_subclasses(graph, s)
+        
+    return subclasses_list
+#%%
+subclasses_list = [NS.LivingThing] + get_subclasses(g, NS.LivingThing)
+print(*subclasses_list, sep="\n")
+#%% md
 # **TASK 7.2: List all individuals of "Person" with RDFLib and SPARQL (remember the subClasses)**
 # 
+#%% md
+# SPARQL version
 #%%
-# TO DO
-# Visualize the results
 q2 = prepareQuery('''
     SELECT ?individual WHERE {
         ?individual rdf:type/rdfs:subClassOf* ns:Person.
@@ -48,11 +62,18 @@ q2 = prepareQuery('''
 for r in g.query(q2):
     print(r.individual)
 #%% md
+# RDFLib version
+#%%
+person_subclasses = [NS.Person] + get_subclasses(g, NS.Person)
+for subclass in person_subclasses:
+    for s, _, _ in g.triples((None, RDF.type, subclass)):
+        print(s)
+#%% md
 # **TASK 7.3: List all individuals of "Person" or "Animal" and all their properties including their class with RDFLib and SPARQL. You do not need to list the individuals of the subclasses of person**
 # 
+#%% md
+# SPARQL version
 #%%
-# TO DO
-# Visualize the results
 q3 = prepareQuery('''
   SELECT ?individual ?thing ?prop ?clas WHERE {
       ?individual rdf:type ?thing.
@@ -66,25 +87,33 @@ q3 = prepareQuery('''
 for r in g.query(q3):
     print(r.individual, r.thing, r.prop, r.clas)
 #%% md
+# RDFLib version
+#%%
+for thing in [NS.Animal, NS.Person]:
+    for s1, p1, o1 in g.triples((None, RDF.type, thing)):
+        for s2, p2, o2 in g.triples((s1, None, None)):
+            print(s2, thing, p2, o2)
+#%% md
 # **TASK 7.4:  List the name of the persons who know Rocky**
 #%%
 # TO DO
 # Visualize the results
 from rdflib.namespace import FOAF
 
-VCARD = Namespace("http://www.w3.org/2001/vcard-rdf/3.0")
+VCARD = Namespace("http://www.w3.org/2001/vcard-rdf/3.0/")
 
 q4 = prepareQuery('''
-    SELECT DISTINCT ?person
+    SELECT DISTINCT ?name
     WHERE {
        ?person foaf:knows ns:RockySmith .
+       ?person vcard:FN ?name .
     }
     ''',
-    initNs = {'ns': NS, 'foaf': FOAF}
+    initNs = {'ns': NS, 'foaf': FOAF, 'vcard': VCARD}
 )
 
 for r in g.query(q4):
-    print(r.person)
+    print(r. name)
 #%% md
 # **Task 7.5: List the entities who know at least two other entities in the graph**
 #%%
@@ -104,4 +133,3 @@ q5 = prepareQuery('''
 
 for r in g.query(q5):
     print(r.entity)
-#%%
