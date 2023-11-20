e8f9fafb4e020af04fc85402cb7aa93046f15f41
Alessandro Rossi <alessandro.rossi.max@gmail.com>
2023-11-20 22:10:25 +0100

    Fixed notebook 7 and removed DS_store

--- Assignment4/AlexRouge - CA43899MT/task07.py
+++ Assignment4/AlexRouge - CA43899MT/task07.py
@@ -35,37 +35,60 @@ q1 = prepareQuery('''
   initNs = { "rdfs":RDFS, "ns":ns}
 )
 # Visualize the results
-print("RDFLib")
-for s, p, o in g.triples((None, RDFS.subClassOf, ns.LivingThing)):
-  print(s)
 print("SPARQL")
 for r in g.query(q1):
   print(r.Subclass)
+
+  def find_subclasses(subclass, subclasses=set()):
+    for s, p, o in g.triples((None, RDFS.subClassOf, subclass)):
+        if s not in subclasses:
+            subclasses.add(s)
+            find_subclasses(s, subclasses)
+    return subclasses
+
+# RDFLib - find all subclasses of ns.LivingThing
+rdfLib_subclasses = find_subclasses(ns.LivingThing)
+print("RDFLib")
+for subclass in rdfLib_subclasses:
+    print(subclass)
 
 """**TASK 7.2: List all individuals of "Person" with RDFLib and SPARQL (remember the subClasses)**
 
 """
 
+def find_individuals_and_subclasses(target_class, individuals=set(), seen_classes=set()):
+    if target_class in seen_classes:
+        return individuals
+
+    seen_classes.add(target_class)
+
+    # Add direct instances of the target class
+    for s, p, o in g.triples((None, RDF.type, target_class)):
+        individuals.add(s)
+
+    # Recursively process each subclass
+    for s, p, o in g.triples((None, RDFS.subClassOf, target_class)):
+        find_individuals_and_subclasses(s, individuals, seen_classes)
+
+    return individuals
+
+individuals_rdflib = find_individuals_and_subclasses(ns.Person)
+print("RDFLib results:")
+for i in individuals_rdflib:
+    print(i)
+
+# SPARQL
 q1 = prepareQuery('''
-  SELECT ?person WHERE {
-    ?subclass rdfs:subClassOf* ns:Person.
-    ?person rdf:type ?subclass.
-  }
-  ''',
-  initNs = { "rdf":RDF, "ns":ns, "rdfs":RDFS}
+    SELECT ?i
+    WHERE {
+        ?i rdf:type/rdfs:subClassOf* ns:Person.
+    }
+    ''',
+    initNs={"rdfs": RDFS, "ns": ns}
 )
-# Visualize the results
-print("RDFLib")
-for s, p, o in g.triples((None, RDF.type, ns.Person)):
-  print(s)
-
-for s, p, o in g.triples((None, RDFS.subClassOf, ns.Person)):
-  for a, b, c in g.triples((None, RDF.type, s)):
-    print(a)
-
-print("SPARQL")
+print("SPARQL results:")
 for r in g.query(q1):
-  print(r.person)
+    print(r.i)
 
 """**TASK 7.3: List all individuals of "Person" or "Animal" and all their properties including their class with RDFLib and SPARQL. You do not need to list the individuals of the subclasses of person**
 
@@ -118,9 +141,27 @@ q2 = prepareQuery('''
 )
 
 # Execute the query and print the results
-print("Persons who know Rocky:")
+print("Persons who know Rocky - SPARQL:")
 for row in g.query(q2):
     print(row.name)
+
+
+# Function to find all persons who know a specific individual
+def find_persons_who_know(target_individual):
+    persons_who_know = []
+    # Querying for all subjects who know the target individual
+    for person in g.subjects(predicate=foaf.knows, object=ns.RockySmith):
+        persons_who_know.append(person)
+    return persons_who_know
+
+# Retrieve the names of persons who know Rocky
+persons_who_know_rocky = find_persons_who_know(ns.RockySmith)
+
+print("Persons who know Rocky - RDFLib:")
+for person in persons_who_know_rocky:
+    name = g.value(subject=person, predicate=vcard.FN)
+    if name:
+        print(name)
 
 """**Task 7.5: List the entities who know at least two other entities in the graph**"""
 
@@ -139,6 +180,23 @@ q2 = prepareQuery('''
 )
 
 # Execute the query and print the results
-print("Persons who know at least two entities:")
+print("Persons who know at least two entities- SPARQL:")
 for row in g.query(q2):
   print(row)
+
+
+  # Function to find entities who know at least two different entities
+def find_entities_knowing_others():
+    entities = set()
+    for entity in g.subjects(predicate=foaf.knows):
+        known_entities = set(g.objects(subject=entity, predicate=foaf.knows))
+        if len(known_entities) >= 2:
+            entities.add(entity)
+    return entities
+
+# Execute the function and print the results
+entities_knowing_others = find_entities_knowing_others()
+
+print("Entities who know at least two other entities- RDFLib:")
+for entity in entities_knowing_others:
+    print(entity)
