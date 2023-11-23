8ae5a0c3b168c65fc47d178097c73bb69324a5c0
cesar-pr-inso <pantoja.rosales.inso@gmail.com>
2023-11-23 22:59:30 +0100

    changes for missing assigments

--- Assignment4/CesarPRinso-223073965/Task06.py
+++ Assignment4/CesarPRinso-223073965/Task06.py
@@ -1,79 +1,74 @@
-# %% [markdown]
-# **Task 06: Modifying RDF(s)**
+# -*- coding: utf-8 -*-
 
-# %%
+# Instalar la biblioteca rdflib si aún no está instalada
+# !pip install rdflib
+
+# URL del almacenamiento en GitHub
 github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2023-2024/master/Assignment4/course_materials"
-# %% [markdown]
-# Leemos el fichero RDF de la forma que lo hemos venido haciendo
 
-# %%
+# Leer el archivo RDF como se muestra en clase
 from rdflib import Graph, Namespace, Literal
-from rdflib.namespace import RDF, RDFS, XSD
+from rdflib.namespace import RDF, RDFS
+
+# Crear el grafo RDF
 g = Graph()
 g.namespace_manager.bind('ns', Namespace("http://somewhere#"), override=False)
 g.namespace_manager.bind('vcard', Namespace("http://www.w3.org/2001/vcard-rdf/3.0#"), override=False)
-g.parse(github_storage+"/rdf/example5.rdf", format="xml")
+g.parse(github_storage + "/rdf/example5.rdf", format="xml")
 
-# %% [markdown]
-# Create a new class named Researcher
+"""Create a new class named Researcher"""
 
-# %%
 ns = Namespace("http://somewhere#")
 g.add((ns.Researcher, RDF.type, RDFS.Class))
 for s, p, o in g:
-  print(s,p,o)
+    print(s, p, o)
 
-# %% [markdown]
 # **TASK 6.1: Create a new class named "University"**
-#
-
-# %%
+ns = Namespace("http://somewhere#")
 g.add((ns.University, RDF.type, RDFS.Class))
 
 # Visualize the results
 for s, p, o in g:
-  print(s,p,o)
+    print(s, p, o)
 
-# %% [markdown]
 # **TASK 6.2: Add "Researcher" as a subclass of "Person"**
-
-# %%
+g.add((ns.Researcher, RDF.type, RDFS.Class))
 g.add((ns.Researcher, RDFS.subClassOf, ns.Person))
 
 # Visualize the results
 for s, p, o in g:
-  print(s,p,o)
+    print(s, p, o)
 
-# %% [markdown]
 # **TASK 6.3: Create a new individual of Researcher named "Jane Smith"**
-
-# %%
 g.add((ns.JaneSmith, RDF.type, ns.Researcher))
 
-# Visualize the results
+# Visualizar los resultados
 for s, p, o in g:
-  print(s,p,o)
+    print(s, p, o)
 
-# %% [markdown]
-# **TASK 6.4: Add to the individual JaneSmith the fullName, given and family names**
-
-# %%
+# **TASK 6.4: Add to the individual JaneSmith the email address, fullName, given and family names**
 VCARD = Namespace("http://www.w3.org/2001/vcard-rdf/3.0#")
-g.add((ns.JaneSmith, VCARD.FN, Literal('Jane Smith', datatype=XSD.string)))
-g.add((ns.JaneSmith, VCARD.Given, Literal('Jane', datatype=XSD.string)))
-g.add((ns.JaneSmith, VCARD.Family, Literal('Smith', datatype=XSD.string)))
+g.add((ns.JaneSmith, VCARD.EMAIL, Literal('jane.smith@example.com', datatype=RDF.XMLLiteral)))
+g.add((ns.JaneSmith, VCARD.FN, Literal('Jane Smith', datatype=RDF.XMLLiteral)))
+g.add((ns.JaneSmith, VCARD.Given, Literal('Jane', datatype=RDF.XMLLiteral)))
+g.add((ns.JaneSmith, VCARD.Family, Literal('Smith', datatype=RDF.XMLLiteral)))
 
 # Visualize the results
 for s, p, o in g.triples((ns.JaneSmith, None, None)):
-  print(s,p,o)
+    print(s, p, o)
 
-# %% [markdown]
 # **TASK 6.5: Add UPM as the university where John Smith works**
-
-# %%
 g.add((ns.UPM, RDF.type, ns.University))
 g.add((ns.JohnSmith, ns.Work, ns.UPM))
 
 # Visualize the results
 for s, p, o in g.triples((None, None, ns.UPM)):
-  print(s,p,o)
+    print(s, p, o)
+
+# **Task 6.6: Add that John knows Jane using the FOAF vocabulary**
+FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+g.add((ns.JohnSmith, FOAF.knows, ns.JaneSmith))
+
+# Visualize the results
+for s, p, o in g.triples((ns.JohnSmith, FOAF.knows, None)):
+    print(s, p, o)
--- Assignment4/CesarPRinso-223073965/Task07.py
+++ Assignment4/CesarPRinso-223073965/Task07.py
@@ -1,98 +1,128 @@
-# %% [markdown]
-# **Task 07: Querying RDF(s)**
+# -*- coding: utf-8 -*-
 
-# %%
+# Instalar la biblioteca rdflib si aún no está instalada
+# !pip install rdflib
+
+# URL del almacenamiento en GitHub
 github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2023-2024/master/Assignment4/course_materials"
-# %% [markdown]
-# Leemos el fichero RDF de la forma que lo hemos venido haciendo
 
-# %%
+# Leer el archivo RDF
 from rdflib import Graph, Namespace, Literal
 from rdflib.namespace import RDF, RDFS
+
+# Crear el grafo RDF
 g = Graph()
 g.namespace_manager.bind('ns', Namespace("http://somewhere#"), override=False)
 g.namespace_manager.bind('vcard', Namespace("http://www.w3.org/2001/vcard-rdf/3.0#"), override=False)
-g.parse(github_storage+"/rdf/example6.rdf", format="xml")
+g.parse(github_storage + "/rdf/example6.rdf", format="xml")
 
-# %% [markdown]
-# **TASK 7.1: List all subclasses of "Person" with RDFLib and SPARQL**
+# **TASK 7.1: List all subclasses of "LivingThing" with RDFLib and SPARQL**
+print("TASK 7.1: List all subclasses of 'LivingThing' with RDFLib and SPARQL")
+print("RDFLib:")
+for s, p, o in g.triples((None, RDF.type, RDFS.Class)):
+    if (o, RDFS.subClassOf, ns.LivingThing) in g:
+        print(o)
 
-# %%
-from rdflib.plugins.sparql import prepareQuery
+print("SPARQL:")
+q1 = """
+    SELECT ?subclass
+    WHERE {
+        ?subclass rdf:type/rdfs:subClassOf* ns:LivingThing .
+    }
+"""
+for row in g.query(q1):
+    print(row.subclass)
 
-ns = Namespace("http://somewhere#")
-
-q1 = prepareQuery('''
-        SELECT ?subclass
-        WHERE {
-            ?subclass rdfs:subClassOf ns:Person
-        }
-    ''',
-    initNs={
-        'ns': ns,
-        'rdfs': RDFS
-    })
-
-# Visualize the results
-for r in g.query(q1):
-    print(r)
-
-# %%
-for subclass in g.subjects(RDFS.subClassOf, ns.Person):
-    print(subclass)
-
-# %% [markdown]
 # **TASK 7.2: List all individuals of "Person" with RDFLib and SPARQL (remember the subClasses)**
-#
+print("\nTASK 7.2: List all individuals of 'Person' with RDFLib and SPARQL")
+print("RDFLib:")
+for s, p, o in g.triples((None, RDF.type, RDFS.Class)):
+    if (o, RDFS.subClassOf, ns.Person) in g:
+        for individual in g.subjects(RDF.type, o):
+            print(individual)
 
-# %%
-q2 = prepareQuery('''
-        SELECT ?individual
-        WHERE {
-            ?subclass rdfs:subClassOf* ns:Person .
-            ?individual a ?subclass
+print("SPARQL:")
+q2 = """
+    SELECT ?individual
+    WHERE {
+        ?individual rdf:type/rdfs:subClassOf* ns:Person .
+    }
+"""
+for row in g.query(q2):
+    print(row.individual)
+
+# **TASK 7.3: List all individuals of "Person" or "Animal" and all their properties including their class with RDFLib and SPARQL. You do not need to list the individuals of the subclasses of person**
+print(
+    "\nTASK 7.3: List all individuals of 'Person' or 'Animal' and all their properties including their class with RDFLib and SPARQL")
+print("RDFLib:")
+for s, p, o in g.triples((None, RDF.type, RDFS.Class)):
+    if (o, RDFS.subClassOf, ns.Person) in g or (o, RDFS.subClassOf, ns.Animal) in g:
+        for individual in g.subjects(RDF.type, o):
+            for p, o in g.predicate_objects(individual):
+                print(f"{individual} {p} {o}")
+
+print("SPARQL:")
+q3 = """
+    SELECT ?individual ?property ?value
+    WHERE {
+        {
+            ?individual rdf:type ns:Person .
+        } UNION {
+            ?individual rdf:type ns:Animal .
         }
-    ''',
-    initNs={
-        'ns': ns,
-        'rdfs': RDFS
-    })
+        ?individual ?property ?value .
+    }
+"""
+for row in g.query(q3):
+    print(row.individual, row.property, row.value)
 
-# Visualize the results
-for r in g.query(q2):
-    print(r)
+# **TASK 7.4:  List the name of the persons who know Rocky**
+print("\nTASK 7.4: List the name of the persons who know Rocky")
+print("RDFLib:")
+for s, p, o in g.triples((None, ns.knows, ns.Rocky)):
+    if (s, RDF.type, ns.Person) in g:
+        for name in g.objects(s, VCARD.FN):
+            print(name)
 
-# %%
-classes = [*g.subjects(RDFS.subClassOf, ns.Person), ns.Person]
-for person_class in classes:
-    for individual in g.subjects(RDF.type, person_class):
-        print(individual)
+print("SPARQL:")
+q4 = """
+    SELECT ?name
+    WHERE {
+        ?person ns:knows ns:Rocky .
+        ?person rdf:type ns:Person .
+        ?person vcard:FN ?name .
+    }
+"""
+for row in g.query(q4):
+    print(row.name)
 
-# %% [markdown]
-# **TASK 7.3: List all individuals of "Person" and all their properties including their class with RDFLib and SPARQL**
-#
+# **Task 7.5: List the entities who know at least two other entities in the graph**
+print("\nTask 7.5: List the entities who know at least two other entities in the graph")
+print("RDFLib:")
+knows_count = {}
+for s, p, o in g.triples((None, ns.knows, None)):
+    if s not in knows_count:
+        knows_count[s] = 1
+    else:
+        knows_count[s] += 1
 
-# %%
-q3 = prepareQuery('''
-        SELECT ?individual ?property ?value
-        WHERE {
-            ?subclass rdfs:subClassOf* ns:Person .
-            ?individual a ?subclass .
-            ?individual ?property ?value
+for entity, count in knows_count.items():
+    if count >= 2:
+        print(entity)
+
+print("SPARQL:")
+q5 = """
+    SELECT ?entity
+    WHERE {
+        {
+            SELECT ?entity (COUNT(?known) as ?count)
+            WHERE {
+                ?entity ns:knows ?known .
+            }
+            GROUP BY ?entity
         }
-    ''',
-    initNs={
-        'ns': ns,
-        'rdfs': RDFS
-    })
-
-# Visualize the results
-for r in g.query(q3):
-    print(r)
-
-# %%
-classes = [*g.subjects(RDFS.subClassOf, ns.Person), ns.Person]
-for person_class in classes:
-    for individual in g.subjects(RDF.type, person_class):
-        for subject, prop, value in g.triples((individual, None, None)):
-            print(subject, prop, value)
+        FILTER (?count >= 2)
+    }
+"""
+for row in g.query(q5):
+    print(row.entity)
